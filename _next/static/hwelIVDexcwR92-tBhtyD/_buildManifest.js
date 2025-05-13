91f80110b2c0e8a67e5f71c49937b14e1d2c14ea
JamesMaxwel <JamesMaxwel@users.noreply.github.com>
2025-05-13 00:26:18 +0000

    deploy: 3676a423aecef71a94a00d97c49664597065da78

---
+++ _next/static/hwelIVDexcwR92-tBhtyD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/cb355538-613b8df283c29562.js","static/chunks/316-47011214cc36ac7e.js","static/chunks/pages/index-7af40c18615db0be.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
